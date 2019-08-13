f3de4c2b3f718966e0e26c1e493f9c3722ffd2f4
Rafael Cupello <rafacupello@gmail.com>
2019-08-13 17:20:15 -0300

    chore: upgrade deps and reorganize project.clj (#94)

--- project.clj
+++ project.clj
@@ -3,29 +3,44 @@
   :url "https://github.com/B2W-Digital/restQL-clojure"
   :license {:name "MIT"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.520"]
-                 [org.clojure/core.async "0.4.490"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/core.async "0.4.500"]
+                 [org.clojure/tools.logging "0.5.0"]
                  [org.clojure/tools.reader "1.3.2"]
                  [aleph "0.4.6"]
                  [r0man/environ "1.1.0"]
                  [instaparse "1.4.10"]
-                 [prismatic/schema "1.1.10"]
-                 [ring/ring-codec "1.1.1"]
-                 [se.haleby/stub-http "0.2.7"]
+                 [prismatic/schema "1.1.12"]
+                 [ring/ring-codec "1.1.2"]
                  [funcool/httpurr "1.1.0"]
                  [funcool/promesa "2.0.1"]
                  [slingshot "0.12.2"]
-                 [metosin/jsonista "0.2.2"]]
-  :hooks [leiningen.cljsbuild]
+                 [metosin/jsonista "0.2.4"]]
+
+  :source-paths ["src/main"]
+  :test-paths ["test/integration" "test/unit"]
+  :resource-paths ["src/resources"]
   :aot [restql.core.api.RestQLJavaApi]
-  :profiles {:test {:dependencies [[se.haleby/stub-http "0.2.7"]]}
-             :uberjar {:aot :all}
-             :auth {#"clojars" {:username :env :password :env}}}
+  :hooks [leiningen.cljsbuild]
+
   :plugins [[lein-ancient "0.6.15"]
             [lein-cloverage "1.1.0"]
             [lein-cljsbuild "1.1.7"]]
+
+  :profiles {:dev [:project/dev]
+             :test [:project/dev]
+             :uberjar {:omit-source true
+                       :aot :all}
+             :auth {#"clojars" {:username :env :password :env}}
+             :project/dev  {:dependencies [[pjstadig/humane-test-output "0.9.0"]
+                                           [se.haleby/stub-http "0.2.7"]]
+                            :plugins      [[com.jakemccrary/lein-test-refresh "0.24.1"]]
+                            :test-refresh {:quiet true
+                                           :changes-only true}
+                            :injections [(require 'pjstadig.humane-test-output)
+                                         (pjstadig.humane-test-output/activate!)]}}
+
   :cljsbuild {:builds [{:id "node"
                         :source-paths ["src"]
                         :compiler {:main restql.core.api.restql
@@ -38,10 +53,8 @@
                                    :pretty-print false
                                    :npm-deps {"uuid" "3.3.2"
                                               "lru-cache" "5.1.1"}}}]}
+
   :deploy-repositories [["clojars"  {:url "https://repo.clojars.org"
                                      :username :env/clojars_username
                                      :password :env/clojars_password
-                                     :sign-releases false}]]
-  :source-paths ["src/main"]
-  :resource-paths ["src/resources"]
-  :test-paths ["test/integration" "test/unit"])
+                                     :sign-releases false}]])
