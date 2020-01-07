ceccedd891c1740b99842264ba6a7e7e557fdfd6
cupello <rafacupello@gmail.com>
2020-01-07 16:01:56 -0300

    bump version to 3.5.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject b2wdigital/restql-core "3.5.5"
+(defproject b2wdigital/restql-core "3.5.6"
   :description "Microservice query language"
   :url "https://github.com/B2W-Digital/restQL-clojure"
   :license {:name "MIT"
